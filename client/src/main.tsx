40298331df37d9b985353aac498c28dabb0b96d2
stevetsang852 <72596467+stevetsang852@users.noreply.github.com>
2026-09-25 13:22:16 +0800

    Load upstream Pokemon card holo CSS.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './card-effects.css';
+import './vendor/pokemon-cards-css/index.css';
 import './ptcg-card.css';
 import './card-tilt';
 
